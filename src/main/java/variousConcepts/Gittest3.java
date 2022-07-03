387f2c9af046d8a2f25f5127d8cb94f3036a179b
rabiaimranusa <rabiaimranusa@yahoo.com>
2022-07-03 01:14:29 -0500

    commit3

---
+++ src/main/java/variousConcepts/Gittest3.java
@@ -0,0 +1,16 @@
+package variousConcepts;
+
+public class Gittest3 {
+
+	public static void main(String[] args) {
+		System.out.println("git test");}
+	
+	public static void anotheretest() {
+		System.out.println(" just practice");
+	}
+	
+	public static void printme() {
+		System.out.println(" just practice again");
+	}
+	
+}
